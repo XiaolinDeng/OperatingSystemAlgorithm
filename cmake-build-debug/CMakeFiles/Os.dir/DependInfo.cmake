cb90648576c72afc3c562cb40c89b68a977d0139
dxl <446304319@qq.com>
2019-03-31 11:43:00 +0800

    set git ignore

--- cmake-build-debug/CMakeFiles/Os.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/Os.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_C
-  "/home/dxl/桌面/Os/PS/PRIORITY.c" "/home/dxl/桌面/Os/cmake-build-debug/CMakeFiles/Os.dir/PS/PRIORITY.c.o"
+  "/home/dxl/CLionProjects/OperatingSystemAlgorithm/PS/PRIORITY.c" "/home/dxl/CLionProjects/OperatingSystemAlgorithm/cmake-build-debug/CMakeFiles/Os.dir/PS/PRIORITY.c.o"
   )
 set(CMAKE_C_COMPILER_ID "GNU")
 
--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/dxl/桌面/Os
+# Install script for directory: /home/dxl/CLionProjects/OperatingSystemAlgorithm
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/dxl/桌面/Os/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/dxl/CLionProjects/OperatingSystemAlgorithm/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
